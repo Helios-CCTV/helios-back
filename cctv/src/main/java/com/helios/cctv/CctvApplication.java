af27f46f0c0aba20f18ffb0c477aad3775edb98b
TreeBomb-01 <150654998+TreeBomb-01@users.noreply.github.com>
2025-08-02 23:09:31 +0900

    스케쥴링 허용
    - @Scheduling을 사용하기 위한 어노테이션 추가

--- cctv/src/main/java/com/helios/cctv/CctvApplication.java
+++ cctv/src/main/java/com/helios/cctv/CctvApplication.java
@@ -3,7 +3,9 @@ package com.helios.cctv;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 @MapperScan("com.helios.cctv.mapper")
 public class CctvApplication {
