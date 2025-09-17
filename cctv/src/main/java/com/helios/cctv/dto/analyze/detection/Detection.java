a47fe7790029ba70e62997a0cb8fecb4ede683fb
TreeBomb-01 <htb010630@gmail.com>
2025-09-17 17:20:17 +0900

    hls 추가 반환
    - hls 추가 반환

--- cctv/src/main/java/com/helios/cctv/dto/analyze/detection/Detection.java
+++ cctv/src/main/java/com/helios/cctv/dto/analyze/detection/Detection.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Detection {
     private int id;                 // cctvId (cctvs.id)
     private String cctvName;        // cctvs.cctvname
+    private String cctvUrl;
     private long analyzeId;         // analyzes.id  ← 추가
     private String date;            // analyzes.analyzed_date → String으로
     private List<String> detections; // detections.damage_type 목록
