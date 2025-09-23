8b85f844f64a76dcba11f6d046ef5712737aa687
TreeBomb-01 <htb010630@gmail.com>
2025-09-23 22:02:18 +0900

    수정

--- cctv/src/main/java/com/helios/cctv/scheduler/AppScheduler.java
+++ cctv/src/main/java/com/helios/cctv/scheduler/AppScheduler.java
@@ -1,5 +1,7 @@
 package com.helios.cctv.scheduler;
 
+import com.helios.cctv.dto.cctv.request.GetCctvRequest;
+import com.helios.cctv.service.CctvIngestService;
 import com.helios.cctv.service.PreprocessJobService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -10,10 +12,25 @@ import org.springframework.stereotype.Component;
 public class AppScheduler {
 
     private final PreprocessJobService preprocessJobService;
+    private final CctvIngestService cctvIngestService;
 
     @Scheduled(cron = "0 30 6 * * *", zone = "Asia/Seoul")
     public void dailyPreprocess() {
         preprocessJobService.allEnqueuePreprocess();
+    }
+
+    @Scheduled(cron = "0 0 6 * * *", zone = "Asia/Seoul")
+    public void dailyUpdateHLS(){
+        GetCctvRequest request = new GetCctvRequest();
+        request.setMinX(125.189f);
+        request.setMaxX(129.584f);
+        request.setMinY(33.569f);
+        request.setMaxY(38.686f);
+        request.setRoadType("ex");   // ex = 고속도로
+        request.setCctvType("4");
+
+        cctvIngestService.updateCctvUrls(request,true);
+        cctvIngestService.updateCctvUrls(request,false);
     }
 
     @Scheduled(cron = "0 * * * * *", zone = "Asia/Seoul")
